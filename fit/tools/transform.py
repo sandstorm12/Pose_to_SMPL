b0500018e3b603790c78e14db46cc306a115d937
sandstorm12 <sandstormeatwo@gmail.com>
2024-01-13 00:12:51 +0330

    Removing the scale optimizer and normalizing the points yourself yields better results

--- fit/tools/transform.py
+++ fit/tools/transform.py
@@ -7,16 +7,16 @@ rotate = {
     'Human3.6M': [-0.001, -0.001, 0.001],
     'NTU': [1., 1., -1.],
     'HAA4D': [1., -1., -1.],
-    'COCO': [1., -1., 1.],
+    'COCO': [-1., -1., 1.],
     'HALPE': [1., -1., 1.],
 }
 
 
 def transform(name, arr: np.ndarray):
     for i in range(arr.shape[0]):
-        origin = (arr[i][11] + arr[i][12]) / 2
+        # origin = (arr[i][11] + arr[i][12]) / 2
         for j in range(arr.shape[1]):
-            arr[i][j] -= origin
+            # arr[i][j] -= origin
             for k in range(3):
                 arr[i][j][k] *= rotate[name][k]
     return arr
